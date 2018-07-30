e2fac88bd80085d3d5aca312d26410ccf3ea4f90
willxm <chenweimin74@gmail.com>
2018-07-30 14:09:51 +0800

    opt 3sums

--- problems/0015_3sum/3sum.go
+++ problems/0015_3sum/3sum.go
@@ -1,108 +1,32 @@
 package problem
 
 import (
-	"fmt"
 	"sort"
 )
 
 func threeSum(nums []int) [][]int {
-	rows := make([][]int, 0)
-	//-1, 0, 1, 2, -1, -4
-	//-4,-1,-1,0,1,2
+	var ret [][]int
 	sort.Ints(nums)
-	l := len(nums)
-	if l < 3 {
-		return nil
-	}
-	i := 0
-	j := l - 1
-	for {
-		if j-i < 2 {
-			break
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i-1] == nums[i] {
+			continue
 		}
-		fmt.Println(i, j)
-		m := nums[i] + nums[j]
-		if m > 0 {
-			k := i + 1
-			for {
-				fmt.Println(k)
-				if k == j {
-					j--
-					for {
-						if i == j {
-							break
-						}
-						if nums[j] == nums[j+1] {
-							j--
-						} else {
-							break
-						}
-					}
-
-					break
-				}
-				if m+nums[k] == 0 {
-					fmt.Println(nums[i], nums[k], nums[j])
-					rows = append(rows, []int{nums[i], nums[k], nums[j]})
-					i++
-					if i == j {
-						break
-					}
-					break
-				}
-				k++
-				for {
-					if k == j {
-						break
-					}
-					if nums[k] == nums[k-1] {
-						k++
-					} else {
-						break
-					}
-				}
+		j, k := i+1, len(nums)-1
+		for j < k {
+			if j > i+1 && nums[j] == nums[j-1] {
+				j++
+				continue
 			}
-		} else {
-			k := j - 1
-			for {
-				fmt.Println(k)
-				if k == i {
-					i++
-					for {
-						if i == j {
-							break
-						}
-						if nums[i] == nums[i-1] {
-							i++
-						} else {
-							break
-						}
-					}
-					break
-				}
-				if m+nums[k] == 0 {
-					fmt.Println(nums[i], nums[k], nums[j])
-					rows = append(rows, []int{nums[i], nums[k], nums[j]})
-
-					j--
-					if i == j {
-						break
-					}
-					break
-				}
+			if nums[i]+nums[j]+nums[k] == 0 {
+				ret = append(ret, []int{nums[i], nums[j], nums[k]})
+				j++
 				k--
-				for {
-					if k == i {
-						break
-					}
-					if nums[k] == nums[k+1] {
-						k--
-					} else {
-						break
-					}
-				}
+			} else if nums[i]+nums[j]+nums[k] < 0 {
+				j++
+			} else {
+				k--
 			}
 		}
 	}
-	return rows
+	return ret
 }
