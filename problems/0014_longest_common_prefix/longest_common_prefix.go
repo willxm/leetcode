c8a43af174fbf89083fb842ac1ed98df57654f26
willxm <chenweimin74@gmail.com>
2018-07-31 17:29:25 +0800

    add longest common prefix

---
+++ problems/0014_longest_common_prefix/longest_common_prefix.go
@@ -0,0 +1,46 @@
+package problem
+
+import (
+	"strings"
+)
+
+func longestCommonPrefix(strs []string) string {
+
+	if len(strs) == 0 {
+		return ""
+	}
+
+	minStr := []rune(getShortestStr(strs))
+
+	var res string
+	tempPrefix := ""
+
+	for _, c := range minStr {
+		tempPrefix += string(c)
+		k := 0
+		for _, v := range strs {
+			if strings.HasPrefix(v, tempPrefix) {
+				k++
+			}
+		}
+		if k == len(strs) {
+			res = tempPrefix
+		} else {
+			return res
+		}
+	}
+	return res
+
+}
+
+func getShortestStr(strs []string) string {
+	minStr := 99999
+	minIndex := -1
+	for k, v := range strs {
+		if len(v) < minStr {
+			minStr = len(v)
+			minIndex = k
+		}
+	}
+	return strs[minIndex]
+}
---
+++ problems/0014_longest_common_prefix/longest_common_prefix_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_longestCommonPrefix(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				strs: []string{"dog", "racecar", "car"},
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonPrefix(tt.args.strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
