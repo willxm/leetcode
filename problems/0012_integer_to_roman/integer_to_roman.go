a2372fab302bec9eabdd8d55ff803c1db39e78aa
willxm <chenweimin74@gmail.com>
2018-07-31 15:38:53 +0800

    add integer to roman

---
+++ problems/0012_integer_to_roman/integer_to_roman.go
@@ -0,0 +1,39 @@
+package problem
+
+type roman struct {
+	symbol string
+	value  int
+}
+
+var romans = []roman{
+	{"M", 1000},
+	{"CM", 900},
+	{"D", 500},
+	{"CD", 400},
+	{"C", 100},
+	{"XC", 90},
+	{"L", 50},
+	{"XL", 40},
+	{"X", 10},
+	{"IX", 9},
+	{"V", 5},
+	{"IV", 4},
+	{"I", 1},
+}
+
+func intToRoman(num int) string {
+	var res string
+	for _, v := range romans {
+		flag := num / v.value
+		if flag > 0 {
+			for i := 0; i < flag; i++ {
+				res += v.symbol
+			}
+			num = num % v.value
+		} else {
+
+		}
+
+	}
+	return res
+}
---
+++ problems/0012_integer_to_roman/integer_to_roman_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_intToRoman(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				num: 1994,
+			},
+			want: "MCMXCIV",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := intToRoman(tt.args.num); got != tt.want {
+				t.Errorf("intToRoman() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
