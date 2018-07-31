0c41576f6c8641ce63802b9bdf1248ff5df0f8e4
willxm <chenweimin74@gmail.com>
2018-07-31 14:28:51 +0800

    add palindrome_number

---
+++ problems/0009_palindrome_number/palindrome_number.go
@@ -0,0 +1,24 @@
+package problem
+
+import (
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	s := strconv.Itoa(x)
+	ps := reverseString(s)
+	if s == ps {
+		return true
+	}
+	return false
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+
+	return string(runes)
+}
