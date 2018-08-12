c78ed19aa90b9c7a8faccd6e0285ae003e16514c
willxm <chenweimin74@gmail.com>
2018-08-12 22:05:06 +0800

    add 0024

---
+++ problems/0024_swap_nodes_in_pairs/swap_nodes_in_pairs.go
@@ -0,0 +1,16 @@
+package problem
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	temp := head.Next
+	head.Next = swapPairs(temp.Next)
+	temp.Next = head
+	return temp
+}
---
+++ problems/0024_swap_nodes_in_pairs/swap_nodes_in_pairs_test.go
@@ -0,0 +1,51 @@
+package problem
+
+import (
+	"reflect"
+	"testing"
+)
+
+func makeListNode(is []int) *ListNode {
+	if len(is) == 0 {
+		return nil
+	}
+
+	res := &ListNode{
+		Val: is[0],
+	}
+	temp := res
+
+	for i := 1; i < len(is); i++ {
+		temp.Next = &ListNode{Val: is[i]}
+		temp = temp.Next
+	}
+
+	return res
+}
+
+func Test_swapPairs(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				makeListNode([]int{1, 2, 3, 4}),
+			},
+			want: makeListNode([]int{2, 1, 4, 3}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := swapPairs(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("swapPairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
