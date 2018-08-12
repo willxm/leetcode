80ce33cf688577151e8c1fd0b0e8822506ca6ac7
willxm <chenweimin74@gmail.com>
2018-08-12 22:25:02 +0800

    add 0026

---
+++ problems/0026_remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,25 @@
+package problem
+
+func removeDuplicates(nums []int) int {
+
+	l := len(nums)
+
+	if l <= 1 {
+		return l
+	}
+
+	res := 1
+	for i := 1; i < len(nums); i++ {
+
+		if nums[i] == nums[i-1] {
+			continue
+		}
+
+		if res != i {
+			nums[res] = nums[i]
+		}
+
+		res++
+	}
+	return res
+}
---
+++ problems/0026_remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
