1861c9d6b25e18dffb805ebf0317bfdcc8df2892
willxm <chenweimin74@gmail.com>
2018-08-13 20:05:41 +0800

    add 0027

---
+++ problems/0027_remove_element/remove_element.go
@@ -0,0 +1,24 @@
+package problem
+
+func removeElement(nums []int, val int) int {
+
+	i, j := 0, len(nums)-1
+
+	for {
+		for i < len(nums) && nums[i] != val {
+			i++
+		}
+
+		for j >= 0 && nums[j] == val {
+			j--
+		}
+
+		if i >= j {
+			break
+		}
+
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	return i
+}
