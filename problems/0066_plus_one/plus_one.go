20af6b139265f30f35b89a5afed5f81b71388d4e
willxm <chenweimin74@gmail.com>
2018-08-15 23:57:17 +0800

    add 0066

---
+++ problems/0066_plus_one/plus_one.go
@@ -0,0 +1,25 @@
+package problem
+
+func plusOne(digits []int) []int {
+	length := len(digits)
+	if length == 0 {
+		return []int{1}
+	}
+
+	digits[length-1]++
+
+	for i := length - 1; i > 0; i-- {
+		if digits[i] < 10 {
+			break
+		}
+		digits[i] -= 10
+		digits[i-1]++
+	}
+
+	if digits[0] > 9 {
+		digits[0] -= 10
+		digits = append([]int{1}, digits...)
+	}
+
+	return digits
+}
---
+++ problems/0066_plus_one/plus_one_test.go
@@ -0,0 +1,33 @@
+package problem
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				digits: []int{4, 3, 2, 1},
+			},
+			want: []int{4, 3, 2, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOne(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
