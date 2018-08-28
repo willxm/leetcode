9edefc6e5ca6fa596384c3c71b498bccddbac8f0
willxm <chenweimin74@gmail.com>
2018-08-28 23:39:18 +0800

    add 0070

---
+++ problems/0070_climbing_stairs/climbing_stairs_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_climbStairs(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				n: 2,
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := climbStairs(tt.args.n); got != tt.want {
+				t.Errorf("climbStairs() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
