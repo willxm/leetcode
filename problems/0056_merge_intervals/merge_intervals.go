9f2cabe2f8f016870dc1865a4ef9a59b789f6e38
willxm <chenweimin74@gmail.com>
2018-08-30 23:30:37 +0800

    oo56

---
+++ problems/0056_merge_intervals/merge_intervals.go
@@ -0,0 +1,57 @@
+package problem
+
+import (
+	"sort"
+)
+
+type Interval struct {
+	Start int
+	End   int
+}
+
+type list []Interval
+
+func merge(intervals []Interval) []Interval {
+
+	if len(intervals) <= 1 {
+		return intervals
+	}
+
+	sort.Sort(list(intervals))
+
+	res := make([]Interval, 0, len(intervals))
+	temp := intervals[0]
+
+	for i := 1; i < len(intervals); i++ {
+
+		if intervals[i].Start <= temp.End {
+			temp.End = max(temp.End, intervals[i].End)
+			continue
+		}
+
+		res = append(res, temp)
+		temp = intervals[i]
+
+	}
+	res = append(res, temp)
+	return res
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func (l list) Len() int {
+	return len(l)
+}
+
+func (l list) Less(i, j int) bool {
+	return l[i].Start < l[j].Start
+}
+
+func (l list) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
---
+++ problems/0056_merge_intervals/merge_intervals_test.go
@@ -0,0 +1,33 @@
+package problem
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		intervals []Interval
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Interval
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				[]Interval{{1, 3}, {2, 6}, {8, 10}, {15, 18}},
+			},
+			want: []Interval{{1, 6}, {8, 10}, {15, 18}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := merge(tt.args.intervals); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("merge() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
