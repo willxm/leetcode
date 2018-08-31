9285f1547bbb80cdd9579a94aee0fa792b08a722
willxm <chenweimin74@gmail.com>
2018-08-31 23:42:07 +0800

    add 0058

---
+++ problems/0058_length_of_last_word/length_of_last_word.go
@@ -0,0 +1,15 @@
+package problem
+
+import (
+	"strings"
+)
+
+func lengthOfLastWord(s string) int {
+	s = strings.TrimSpace(s)
+	l := len(s)
+	if l == 0 {
+		return 0
+	}
+	ss := strings.Split(s, " ")
+	return len(ss[len(ss)-1])
+}
---
+++ problems/0058_length_of_last_word/length_of_last_word_test.go
@@ -0,0 +1,37 @@
+package problem
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				s: "Hello World",
+			},
+			want: 5,
+		},
+		{
+			name: "case2",
+			args: args{
+				s: "a ",
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
