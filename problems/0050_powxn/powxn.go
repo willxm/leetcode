fe145e90455ef0778b3d1ea5af9ff2403117808f
willxm <chenweimin74@gmail.com>
2018-09-03 19:13:26 +0800

    0050

---
+++ problems/0050_powxn/powxn.go
@@ -0,0 +1,24 @@
+package problem
+
+func myPow(x float64, n int) float64 {
+	f := 0
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	if n < 0 {
+		n = -n
+		f = 1
+	}
+	res := myPow(x, n>>1)
+	res *= res
+	if n&0x1 == 1 {
+		res *= x
+	}
+	if f == 1 {
+		return 1.0 / res
+	}
+	return res
+}
---
+++ problems/0050_powxn/powxn_test.go
@@ -0,0 +1,40 @@
+package problem
+
+import "testing"
+
+func Test_myPow(t *testing.T) {
+	type args struct {
+		x float64
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				x: 2.0,
+				n: 2,
+			},
+			want: 4.0,
+		},
+		{
+			name: "case2",
+			args: args{
+				x: 2.0,
+				n: -2,
+			},
+			want: 0.25,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myPow(tt.args.x, tt.args.n); got != tt.want {
+				t.Errorf("myPow() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
