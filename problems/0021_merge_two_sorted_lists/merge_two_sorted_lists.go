cf5299100e284a11e0d4961708f6aa194ab7b612
willxm <chenweimin74@gmail.com>
2018-09-04 19:40:18 +0800

    add recursion func

--- problems/0021_merge_two_sorted_lists/merge_two_sorted_lists.go
+++ problems/0021_merge_two_sorted_lists/merge_two_sorted_lists.go
@@ -14,6 +14,26 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 		return l1
 	}
 
+	var mergeHead *ListNode
+
+	if l1.Val < l2.Val {
+		mergeHead = l1
+		mergeHead.Next = mergeTwoLists(l1.Next, l2)
+	} else {
+		mergeHead = l2
+		mergeHead.Next = mergeTwoLists(l1, l2.Next)
+	}
+	return mergeHead
+}
+
+func mergeTwoListss(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
 	var head, node *ListNode
 	if l1.Val < l2.Val {
 		head = l1
