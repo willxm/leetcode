085195a649ef6bda4273af6b2c32756202efe975
willxm <chenweimin74@gmail.com>
2018-09-04 20:12:33 +0800

    add Linked List kit

--- kits/linked_list.go
+++ kits/linked_list.go
@@ -9,16 +9,13 @@ func makeListNode(is []int) *ListNode {
 	if len(is) == 0 {
 		return nil
 	}
-
 	res := &ListNode{
 		Val: is[0],
 	}
 	temp := res
-
 	for i := 1; i < len(is); i++ {
 		temp.Next = &ListNode{Val: is[i]}
 		temp = temp.Next
 	}
-
 	return res
 }
