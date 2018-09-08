50954fe4b11ad0dd64a592c5b27532c72296decf
willxm <chenweimin74@gmail.com>
2018-09-08 13:26:35 +0800

    add 0062

---
+++ problems/0062_unique_paths/unique_paths.go
@@ -0,0 +1,25 @@
+package problem
+
+func uniquePaths(m int, n int) int {
+	sum := m + n
+	m = max(m, n)
+	n = sum - m
+	return C(m-1+n-1, n-1)
+}
+
+func C(m, n int) int {
+	x := 1
+	y := 1
+	for i := 0; i < n; i++ {
+		x *= m - i
+		y *= i + 1
+	}
+	return x / y
+}
+
+func max(m, n int) int {
+	if m > n {
+		return m
+	}
+	return n
+}
