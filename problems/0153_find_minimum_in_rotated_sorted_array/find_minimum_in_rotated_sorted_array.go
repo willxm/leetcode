a58d26c85f13a16bd2fd4586b5325e7ca55a3d9c
willxm <chenweimin74@gmail.com>
2018-09-10 17:13:06 +0800

    add 0153

---
+++ problems/0153_find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array.go
@@ -0,0 +1,36 @@
+package problem
+
+func findMin(nums []int) int {
+	size := len(nums)
+	if size == 0 {
+		return 0
+	} else if size == 1 {
+		return nums[0]
+	} else if size == 2 {
+		return min(nums[0], nums[1])
+	}
+
+	start := 0
+	stop := size - 1
+
+	for start < stop-1 {
+		if nums[start] < nums[stop] {
+			return nums[start]
+		}
+
+		mid := start + (stop-start)/2
+		if nums[mid] > nums[start] {
+			start = mid
+		} else if nums[mid] < nums[start] {
+			stop = mid
+		}
+	}
+	return min(nums[start], nums[stop])
+}
+
+func min(m, n int) int {
+	if m > n {
+		return n
+	}
+	return m
+}
---
+++ problems/0153_find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_findMin(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				nums: []int{4, 5, 6, 7, 0, 1, 2},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMin(tt.args.nums); got != tt.want {
+				t.Errorf("findMin() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
