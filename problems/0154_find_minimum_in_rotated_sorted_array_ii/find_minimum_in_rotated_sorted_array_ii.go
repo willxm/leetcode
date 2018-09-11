6de8289d2e0d1646dd8f678dfaacc3b3bdd94814
willxm <chenweimin74@gmail.com>
2018-09-11 20:09:32 +0800

    add 154

---
+++ problems/0154_find_minimum_in_rotated_sorted_array_ii/find_minimum_in_rotated_sorted_array_ii.go
@@ -0,0 +1,39 @@
+package problem
+
+func findMin(nums []int) int {
+	size := len(nums)
+
+	if size == 0 {
+		return 0
+	} else if size == 1 {
+		return nums[0]
+	} else if size == 2 {
+		return min(nums[0], nums[1])
+	}
+
+	start := 0
+	stop := size - 1
+
+	for start < stop-1 {
+		if nums[start] < nums[stop] {
+			return nums[start]
+		}
+
+		mid := start + (stop-start)/2
+		if nums[start] < nums[mid] {
+			start = mid
+		} else if nums[start] > nums[mid] {
+			stop = mid
+		} else {
+			start++
+		}
+	}
+	return min(nums[start], nums[stop])
+}
+
+func min(m, n int) int {
+	if m > n {
+		return n
+	}
+	return m
+}
