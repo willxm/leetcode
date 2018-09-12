748b9f36802aca8c63a5327b9c1d4db1dc1b8d12
willxm <chenweimin74@gmail.com>
2018-09-12 19:38:37 +0800

    0162

---
+++ problems/0162_find_peak_element/find_peak_element.go
@@ -0,0 +1,25 @@
+package problem
+
+func findPeakElement(nums []int) int {
+	size := len(nums)
+	if size == 0 {
+		return 0
+	} else if size == 1 {
+		return 0
+	} else if size == 2 {
+		if nums[0] > nums[1] {
+			return 0
+		} else {
+			return 1
+		}
+	}
+	for i := 1; i < size; i++ {
+		if i == size-1 && nums[i] > nums[i-1] {
+			return size - 1
+		}
+		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
+			return i
+		}
+	}
+	return 0
+}
