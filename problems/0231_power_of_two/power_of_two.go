f364164d824d8395d2fb68f18325b10fd387b253
willxm <chenweimin74@gmail.com>
2018-09-14 22:26:31 +0800

    add 0231

---
+++ problems/0231_power_of_two/power_of_two.go
@@ -0,0 +1,19 @@
+package problem
+
+func isPowerOfTwo(n int) bool {
+	if n < 0 {
+		return false
+	}
+	hasOne := false
+	for n > 0 {
+		if (n & 1) != 0 {
+			if hasOne {
+				return false
+			} else {
+				hasOne = true
+			}
+		}
+		n >>= 1
+	}
+	return hasOne
+}
---
+++ problems/0231_power_of_two/power_of_two_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_isPowerOfTwo(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				n: 4,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfTwo(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
