06dc8f16817d1ca27ba493811dcfefa6db823a44
willxm <chenweimin74@gmail.com>
2018-09-16 23:51:13 +0800

    add 0179

---
+++ problems/0179_largest_number/largest_number.go
@@ -0,0 +1,60 @@
+package problem
+
+import (
+	"sort"
+	"strconv"
+)
+
+func largestNumber(nums []int) string {
+
+	tNums := typeInt(nums)
+
+	sort.Sort(tNums)
+
+	res := ""
+
+	first := tNums[0]
+
+	l := len(tNums)
+
+	if l == 1 {
+		return strconv.Itoa(tNums[0])
+	}
+
+	for i := 0; i < l; i++ {
+
+		if first == 0 {
+			if i == l-1 {
+				return "0"
+			}
+			first = tNums[i+1]
+			continue
+		}
+
+		res += strconv.Itoa(tNums[i])
+	}
+
+	return res
+
+}
+
+type typeInt []int
+
+func (t typeInt) Len() int {
+	return len(t)
+}
+
+func (t typeInt) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t typeInt) Less(i, j int) bool {
+	stri := strconv.Itoa(t[i])
+	strj := strconv.Itoa(t[j])
+
+	str1 := stri + strj
+
+	str2 := strj + stri
+
+	return str1 > str2
+}
---
+++ problems/0179_largest_number/largest_number_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_largestNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				nums: []int{0, 0},
+			},
+			want: "0",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestNumber(tt.args.nums); got != tt.want {
+				t.Errorf("largestNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
