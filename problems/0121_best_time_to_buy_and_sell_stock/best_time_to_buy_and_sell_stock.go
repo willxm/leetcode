e979b2b0258ab7c3820e9a32e69c2856619e4915
willxm <chenweimin74@gmail.com>
2018-09-16 16:47:15 +0800

    add 0121

---
+++ problems/0121_best_time_to_buy_and_sell_stock/best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,36 @@
+package problem
+
+func maxProfit(prices []int) int {
+	l := len(prices)
+	if l < 2 {
+		return 0
+	}
+
+	minP := prices[0]
+
+	profit := prices[1] - prices[0]
+
+	for i := 2; i < l; i++ {
+		minP = min(minP, prices[i-1])
+		profit = max(profit, prices[i]-minP)
+	}
+
+	if profit < 0 {
+		return 0
+	}
+	return profit
+}
+
+func min(m, n int) int {
+	if m < n {
+		return m
+	}
+	return n
+}
+
+func max(m, n int) int {
+	if m > n {
+		return m
+	}
+	return n
+}
