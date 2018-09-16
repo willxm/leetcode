22b3721a43bfced28e081529e62ca83a107973e8
willxm <chenweimin74@gmail.com>
2018-09-16 18:27:28 +0800

    add 0152

---
+++ problems/0152_maximum_product_subarray/0152_maximum_product_subarray.go
@@ -0,0 +1,37 @@
+package problem
+
+func maxProduct(nums []int) int {
+	l := len(nums)
+	if l == 0 {
+		return 0
+	} else if l == 1 {
+		return nums[0]
+	}
+
+	p := nums[0]
+	minP := nums[0]
+	maxP := nums[0]
+
+	for i := 1; i < l; i++ {
+
+		tmp := maxP
+		maxP = max(max(maxP*nums[i], nums[i]), minP*nums[i])
+		minP = min(min(tmp*nums[i], nums[i]), minP*nums[i])
+		p = max(maxP, p)
+	}
+	return p
+}
+
+func min(m, n int) int {
+	if m < n {
+		return m
+	}
+	return n
+}
+
+func max(m, n int) int {
+	if m > n {
+		return m
+	}
+	return n
+}
---
+++ problems/0152_maximum_product_subarray/0152_maximum_product_subarray_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_maxProduct(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				nums: []int{2, 3, -2, 4},
+			},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProduct(tt.args.nums); got != tt.want {
+				t.Errorf("maxProduct() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
