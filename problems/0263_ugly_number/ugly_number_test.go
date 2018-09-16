317cbaffcd1b82b13519e321f1edcf2cb9e936b4
willxm <chenweimin74@gmail.com>
2018-09-16 21:29:38 +0800

    add 0263

---
+++ problems/0263_ugly_number/ugly_number_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_isUgly(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				num: -2147483648,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isUgly(tt.args.num); got != tt.want {
+				t.Errorf("isUgly() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
