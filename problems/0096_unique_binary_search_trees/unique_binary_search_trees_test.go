3160c6556cfbdaf9bf005162a206d1bcfd5d13b4
willxm <chenweimin74@gmail.com>
2018-09-21 19:40:03 +0800

    add 0096

---
+++ problems/0096_unique_binary_search_trees/unique_binary_search_trees_test.go
@@ -0,0 +1,31 @@
+package problem
+
+import "testing"
+
+func Test_numTrees(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+
+			name: "case1",
+			args: args{
+				n: 3,
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numTrees(tt.args.n); got != tt.want {
+				t.Errorf("numTrees() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
