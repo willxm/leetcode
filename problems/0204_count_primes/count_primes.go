115f155aab87e0939e07006f386663b1a51e4423
willxm <chenweimin74@gmail.com>
2018-10-02 15:52:16 +0800

    ff

---
+++ problems/0204_count_primes/count_primes.go
@@ -0,0 +1,41 @@
+package problem
+
+import "runtime"
+
+func GenerateNatural() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 2; ; i++ {
+			ch <- i
+		}
+	}()
+	return ch
+}
+
+func PrimeFilter(in <-chan int, prime int) chan int {
+	out := make(chan int)
+	go func() {
+		for {
+			if i := <-in; i%prime != 0 {
+				out <- i
+			}
+		}
+	}()
+	return out
+}
+
+func countPrimes(n int) int {
+	runtime.GOMAXPROCS(1)
+	var ret int
+	ch := GenerateNatural()
+	for i := 0; i < n; i++ {
+		prime := <-ch
+		ch = PrimeFilter(ch, prime)
+		if prime < n {
+			ret++
+		} else {
+			return ret
+		}
+	}
+	return ret
+}
