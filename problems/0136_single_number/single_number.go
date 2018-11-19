2a1ab60a4d5fce00dbf1e9201a76bf28c0746315
willxm <chenweimin74@gmail.com>
2018-11-19 23:54:19 +0800

    add 0136

---
+++ problems/0136_single_number/single_number.go
@@ -0,0 +1,9 @@
+package problem
+
+func singleNumber(nums []int) int {
+	res := 0
+	for _, v := range nums {
+		res ^= v
+	}
+	return res
+}
---
+++ problems/0136_single_number/single_number_test.go
@@ -0,0 +1,30 @@
+package problem
+
+import "testing"
+
+func Test_singleNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				nums: []int{4, 1, 2, 2, 1},
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := singleNumber(tt.args.nums); got != tt.want {
+				t.Errorf("singleNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
