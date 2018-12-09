27ee9b550e0c3d8d372218aa8164cc239b9e31f3
willxm <chenweimin74@gmail.com>
2018-12-09 12:55:39 +0800

    add 0142

---
+++ problems/0142_linked_list_cycle_ii/linked_list_cycle_ii.go
@@ -0,0 +1,32 @@
+package problem
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	ptr1, ptr2 := head, fast
+	for ptr1 != ptr2 {
+		ptr1 = ptr1.Next
+		ptr2 = ptr2.Next
+	}
+	return ptr1
+}
