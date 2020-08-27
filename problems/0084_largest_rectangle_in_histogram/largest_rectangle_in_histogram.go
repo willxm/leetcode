52f7fc0d74f33bd08304a433d7950851cfc90e7d
willxm <chenweimin74@gmail.com>
2020-08-28 00:14:28 +0800

    add 084

--- problems/0084_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
+++ problems/0084_largest_rectangle_in_histogram/largest_rectangle_in_histogram.go
@@ -1,5 +1,38 @@
 package problem
 
 func largestRectangleArea(heights []int) int {
-	return 0
+	var maxArea int
+	// var minHeight int
+
+	// maxHeight := heights[0]
+
+	if len(heights) == 1 {
+		return heights[0]
+	}
+
+	for i := 0; i < len(heights); i++ {
+		tmpMax := heights[i] * 1
+		tmpMinHeight := heights[i]
+
+		if i != 0 {
+			if heights[i-1] > heights[i] {
+				continue
+			}
+		}
+
+		for j := i + 1; j < len(heights); j++ {
+
+			if heights[j] < tmpMinHeight {
+				tmpMinHeight = heights[j]
+			}
+
+			if tmpMax < tmpMinHeight*(j-i+1) {
+				tmpMax = tmpMinHeight * (j - i + 1)
+			}
+		}
+		if maxArea < tmpMax {
+			maxArea = tmpMax
+		}
+	}
+	return maxArea
 }
---
+++ problems/0084_largest_rectangle_in_histogram/largest_rectangle_in_histogram_test.go
@@ -0,0 +1,45 @@
+package problem
+
+import "testing"
+
+func Test_largestRectangleArea(t *testing.T) {
+	type args struct {
+		heights []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				heights: []int{2, 1, 5, 6, 2, 3},
+			},
+			want: 10,
+		},
+		{
+			name: "case2",
+			args: args{
+				heights: []int{4, 2},
+			},
+			want: 4,
+		},
+		{
+			name: "case3",
+			args: args{
+				heights: []int{4, 2, 0, 3, 2, 5},
+			},
+			want: 6,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestRectangleArea(tt.args.heights); got != tt.want {
+				t.Errorf("largestRectangleArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
