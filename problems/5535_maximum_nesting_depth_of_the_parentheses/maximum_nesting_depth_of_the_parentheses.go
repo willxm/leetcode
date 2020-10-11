fc340d861e80a49341bb8dece8191bf6abc61484
willxm <chenweimin74@gmail.com>
2020-10-11 10:51:00 +0800

    add week 210

---
+++ problems/5535_maximum_nesting_depth_of_the_parentheses/maximum_nesting_depth_of_the_parentheses.go
@@ -0,0 +1,23 @@
+package problem
+
+func maxDepth(s string) int {
+	rs := []rune(s)
+
+	maxLen := 0
+	tmpLen := 0
+
+	for _, v := range rs {
+		if v == '(' {
+			tmpLen++
+		}
+
+		if v == ')' {
+			tmpLen--
+		}
+
+		if tmpLen > maxLen {
+			maxLen = tmpLen
+		}
+	}
+	return maxLen
+}
