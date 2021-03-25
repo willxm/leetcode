c390ab72d16faa4bf9b3f25e2728be10e6c66350
willxm <willxm.chen@gmail.com>
2021-03-25 23:58:23 +0800

    add 0088

---
+++ problems/0088_merge_sorted_array/merge_sorted_array.go
@@ -0,0 +1,27 @@
+package problems
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	temp := make([]int, m)
+	copy(temp, nums1)
+
+	j, k := 0, 0
+	for i := 0; i < len(nums1); i++ {
+		if k >= n {
+			nums1[i] = temp[j]
+			j++
+			continue
+		}
+		if j >= m {
+			nums1[i] = nums2[k]
+			k++
+			continue
+		}
+		if temp[j] < nums2[k] {
+			nums1[i] = temp[j]
+			j++
+		} else {
+			nums1[i] = nums2[k]
+			k++
+		}
+	}
+}
