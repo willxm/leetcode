fcc9541d4674b301681b9103b30924c8c1e2136f
willxm <willxm.chen@gmail.com>
2021-03-29 23:47:02 +0800

    add 00326

---
+++ problems/0326_power_of_three/power_of_three_test.go
@@ -0,0 +1,30 @@
+package problems
+
+import "testing"
+
+func Test_isPowerOfThree(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "case1",
+			args: args{
+				n: 27,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfThree(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfThree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
