130d1c6ce3ada31328a1ea145c4a3d637bc98267
willxm <willxm.chen@gmail.com>
2021-04-02 23:33:40 +0800

    opt 0326

--- problems/0326_power_of_three/power_of_three.go
+++ problems/0326_power_of_three/power_of_three.go
@@ -1,6 +1,6 @@
 package problems
 
-func isPowerOfThree(n int) bool {
+func isPowerOfThree1(n int) bool {
 	if n < 1 {
 		return false
 	}
@@ -10,4 +10,8 @@ func isPowerOfThree(n int) bool {
 	}
 
 	return n == 1
+}
+
+func isPowerOfThree(n int) bool {
+	return n > 0 && 1162261467%n == 0
 }
