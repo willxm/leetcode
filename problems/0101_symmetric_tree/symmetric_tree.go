e034fe5bed363fb421833d9d020ce4be9428c4b4
willxm <willxm.chen@gmail.com>
2021-04-05 23:33:26 +0800

    commit 0101

--- problems/0101_symmetric_tree/symmetric_tree.go
+++ problems/0101_symmetric_tree/symmetric_tree.go
@@ -20,6 +20,7 @@ func isSameTree(left, right *TreeNode) bool {
 	if left == nil || right == nil {
 		return false
 	}
+	//如果左树的左孩子与右树的右孩子对称，左树的右孩子与右树的左孩子对称，那么这个左树和右树就对称。
 	sl := isSameTree(left.Left, right.Right)
 	sr := isSameTree(left.Right, right.Left)
 	return left.Val == right.Val && sl && sr
