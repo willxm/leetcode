78360ec185768aa26d5dd2fe85f2c8c7cafc426a
willxm <willxm.chen@gmail.com>
2021-04-13 00:40:37 +0800

    opt 0287

--- problems/0287_find_the_duplicate_number/find_the_duplicate_number.go
+++ problems/0287_find_the_duplicate_number/find_the_duplicate_number.go
@@ -11,3 +11,17 @@ func findDuplicate(nums []int) int {
 	}
 	return 0
 }
+
+func findDuplicate1(a []int) int {
+	slow, fast := a[0], a[a[0]]
+	for slow != fast {
+		slow, fast = a[slow], a[a[fast]]
+	}
+
+	slow = 0
+	for slow != fast {
+		slow, fast = a[slow], a[fast]
+	}
+
+	return slow
+}
